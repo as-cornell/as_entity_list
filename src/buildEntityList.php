a2dbe9e94d82583bf181f86fd8103bd8ab84e78f
markewilson <mark.e.wilson@gmail.com>
2024-10-23 15:28:03 -0400

    adds several new functions namespaced for as

--- src/buildEntityList.php
+++ src/buildEntityList.php
@@ -47,25 +47,41 @@ class buildEntityList extends \Twig\Extension\AbstractExtension
       }
     //article nids filtered by tags
     if ($type == 'article'){
-        $entity_list = as_entity_list_get_articles($type,$count,$tags);
+        $entity_list = as_entity_list_get_articles($count,$tags);
+      }
+    //article nids filtered by tags
+    if ($type == 'article_as'){
+        $entity_list = as_entity_list_get_articles_as($count,$tags);
       }
     // article nids to display in footer of article, excludes current nid
     if ($type == 'article_footer'){
-        $entity_list = as_entity_list_get_articles_footer($type,$count,$nid);
+        $entity_list = as_entity_list_get_articles_footer($count,$nid);
       }
     // article nids to display in footer of person, filtered by person nid
     if ($type == 'article_person'){
         $entity_list = as_entity_list_get_articles_person($count,$nid);
       }
+    if ($type == 'article_person_as'){
+        $entity_list = as_entity_list_get_articles_person($count,$nid);
+      }
+    if ($type == 'article_department'){
+        $entity_list = as_entity_list_get_articles_department($count,$nid);
+      }
     // people nids filtered by tags, type is used to filter results by vocabulary
     if ($type == 'person'){
-        $entity_list = as_entity_list_get_people($type,$count,$tags);
+        $entity_list = as_entity_list_get_people($count,$tags);
+      }
+    if ($type == 'person_department'){
+        $entity_list = as_entity_list_get_people_department($count,$tags);
+      }
+    if ($type == 'person_research'){
+        $entity_list = as_entity_list_get_people_research($count,$tags);
       }
     if ($type == 'work'){
-        $entity_list = as_entity_list_get_works($type,$count,$tags);
+        $entity_list = as_entity_list_get_works($count,$tags);
       }
     if ($type == 'cinevent'){
-        $entity_list = as_entity_list_get_cinevents($type,$count,$tags);
+        $entity_list = as_entity_list_get_cinevents($count,$tags);
       }
     return $entity_list;
   }
